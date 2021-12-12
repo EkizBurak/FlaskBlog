94ab958026e084b431634091328a7147adca3d9b
EkizBurak <86228168+EkizBurak@users.noreply.github.com>
2021-12-12 22:40:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ app.config['MAIL_PASSWORD'] = '3F2C9BDF0F802B4082F159ACF6332B4C4AA9F82D68B70106A
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
 s = URLSafeTimedSerializer('Thisisasecret!')
+
 def sendEmail(title,recipients,message):
     msg = Message(title, sender='buraksamp34@gmail.com', recipients=[recipients])
     msg.body = message
@@ -28,9 +30,10 @@ def sendEmail(title,recipients,message):
         return True
     except:
         return False
+
 def checkUsername(username):
-    sonuc=list(sql.execute(f"select username from users where username='{username}'"))
-    if len(sonuc)==0:
+    check=list(sql.execute(f"select username from users where username='{username}'"))
+    if len(check)==0:
         return True
     else:
         return False
@@ -38,14 +41,14 @@ def checkUsername(username):
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/register')
-def ormliKayit():
-   return render_template('register.html')
+@app.route('/sign-up')
+def signUp():
+    return render_template('signup.html')
 
-@app.route('/new-register',methods = ['POST', 'GET'])
-def addrec():
+@app.route('/new-sign-up',methods = ['POST', 'GET'])
+def newSignUp():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -53,20 +56,23 @@ def addrec():
         firstName = request.form['firstName']
         lastName = request.form['lastName']
         eMail = request.form['eMail']
-
+        try:
+            privacyPolicy = request.form['privacyPolicy']
+        except:
+            return render_template("register.html", err="Please Confirm Privacy Policy", username=username, firstName=firstName,lastName=lastName, eMail=eMail)
 
         if username=="":
-            return render_template("errormsg.html", err="Username cannot be blank")
+            return render_template("register.html", err="Username cannot be blank", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
         elif firstName=="":
-            return render_template("errormsg.html", err="First Name cannot be blank")
+            return render_template("register.html", err="First Name cannot be blank", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
         elif lastName=="":
-            return render_template("errormsg.html", err="Last Name cannot be blank")
+            return render_template("register.html", err="Last Name cannot be blank", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
         elif eMail=="":
-            return render_template("errormsg.html", err="E-Mail cannot be blank")
+            return render_template("register.html", err="E-Mail cannot be blank", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
         elif password=="" or password2=="":
-            return render_template("errormsg.html", err="Password cannot be blank")
+            return render_template("register.html", err="Password cannot be blank", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
         elif password!=password2:
-            return render_template("errormsg.html", err="Passwords not match")
+            return render_template("register.html", err="Passwords not match", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
 
         if checkUsername(username):
             password = hashlib.sha256(password.encode('ascii')).hexdigest()
@@ -81,6 +87,7 @@ def addrec():
 
 
             resp = make_response(render_template('register.html'))
+
             resp.set_cookie('usernameCookie', username)
             resp.set_cookie('passwordCookie',  password)
             resp.set_cookie('firstNameCookie', firstName)
@@ -88,9 +95,9 @@ def addrec():
             resp.set_cookie('eMailCookie', eMail)
             resp.set_cookie('saltCookie', salt)
 
-            return resp
+            return render_template("register.html", err="Please Confirm Your E-Mail Address", deneme=resp)
         else:
-            return render_template("errormsg.html", err="Username already exit")
+            return render_template("register.html", err="Username already exit", username=username, firstName=firstName, lastName=lastName, eMail=eMail)
 
 
 @app.route('/confirm_email/<token>')
@@ -105,14 +112,15 @@ def confirm_email(token):
     lastName = request.cookies.get('lastNameCookie')
     eMail= request.cookies.get('eMailCookie')
     salt = request.cookies.get('saltCookie')
-    print(username,password,firstName,lastName,eMail,salt)
+
     if checkUsername(username):
-        pass
-        #sql.execute(f"insert into users values('{username}', '{password}', '{firstName}', '{lastName}', '{eMail}', '{salt}')")
-        #sql.commit()
+        sql.execute(f"insert into users values('{username}', '{password}', '{firstName}', '{lastName}', '{eMail}', '{salt}')")
+        sql.commit()
+
+
     else:
-        return render_template("errormsg.html", err="Username already exit")
-    return '<h1>The token works!</h1>'
+        return render_template("register.html", err="Username already exit")
+    return render_template("home.html", err="delete signup cookies")
 
 
 
